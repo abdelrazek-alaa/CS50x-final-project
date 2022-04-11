4d4880a09e75bc2857bda919d7dc1cecede5cd5e
abdelrazek <abdelrazekhdd@gmail.com>
2022-04-11 12:01:31 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-yellow-800">Hello World!</div>;
+  return (
+    <div className="font-spartan container mx-auto bg-light-grayish-cyan">
+      <header className="bg-mobile md:bg-desktop bg-dark-cyan bg-no-repeat bg-cover h-24">
+        <h2 className="text-light-grayish-cyan text-center text-2xl leading-[4]">
+          Awesome Learning Resources
+        </h2>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spartan: ["Spartan", "sans-serif"],
+      },
+      colors: {
+        "dark-cyan": "hsl(180, 29%, 50%)",
+        "light-grayish-cyan": " hsl(180, 52%, 96%)",
+        "dark-grayish-cyan": "hsl(180, 8%, 52%)",
+        "very-dark-grayish-cyan": "hsl(180, 14%, 20%)",
+      },
+      backgroundImage: {
+        desktop: "url('/public/images/bg-header-desktop.svg')",
+        mobile: "url('/public/images/bg-header-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };
